2f4322a78b04523140f386c06c4976a9e4c7a457
Xinwu Cheng <xinwu@yahoo-inc.com>
2017-04-17 22:42:34 -0700

    add dependencies in setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="finance",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Xinwu",
+
+    # Packages
+    # packages=["finance"],
+
+    # Include additional files into the package
+    # include_package_data=True,
+
+    # Details
+    # url="http://pypi.python.org/pypi/MyApplication_v010/",
+
+    #
+    # license="LICENSE.txt",
+    # description="Useful towel-related stuff.",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "Bottleneck",
+        "matplotlib",
+        "numexpr",
+        "numpy",
+        "pandas",
+        "pandas-datareader",
+        "scipy",
+        "xlrd"
+    ],
+)
