27e0128edf1b75422104f9020fa9de54b8a1de16
Xinwuc <xinwucheng@gmail.com>
2017-12-20 18:32:59 -0800

    update install dependency

--- src/strategy/sell/sell_strategy_lock_profit.py
+++ src/strategy/sell/sell_strategy_lock_profit.py
@@ -1,5 +1,4 @@
 import pandas
-from zipline.finance.execution import ExecutionStyle, StopLimitOrder
 
 from utility.data_utility import DataUtility
 from utility.utility import *
@@ -19,11 +18,3 @@ class SimpleProfitLockSellStrategy:
         sell_price = price_history[StockPriceField.Low.value][target_date] * (
                 1 - price_history['daily_range_pct'].mean())
         return sell_price
-
-    def get_sell_order(self, profit_lock_price: float, cost_basis: float,
-                       previous_lock_price: float = 0) -> ExecutionStyle:
-        minimal_sell_price = cost_basis * (1 + self.minimal_profit)
-        order = None
-        if profit_lock_price >= minimal_sell_price and profit_lock_price > previous_lock_price:
-            order = StopLimitOrder(limit_price=profit_lock_price, stop_price=profit_lock_price)
-        return order
