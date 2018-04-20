a2ba657f15fd29844bcefac610a82acfd2ae9e4b
Xinwuc <xinwucheng@gmail.com>
2018-04-20 12:38:11 -0700

    add low sell target in day runner

--- src/robinhood/day_runner.py
+++ src/robinhood/day_runner.py
@@ -34,17 +34,18 @@ class RobinhoodDayRunner:
         return parser.parse_args()
 
     def run(self):
-        sell_book = pd.read_csv('configs/robinhood_sell_book.csv', index_col=0).ix[:, 0].to_dict()
+        sell_book = pd.read_csv('configs/robinhood_sell_book.csv', index_col=0)
         positions = self.robinhood.get_positions()
-        symbols = set(positions.keys()).intersection(sell_book.keys())
+        symbols = set(positions.keys()).intersection(sell_book.index)
         if not symbols:
-            self.logger.info('No positions to run, runbook: %d, positions: %d' % (len(sell_book), len(positions)))
+            self.logger.info('No positions to run, sell book: %d, positions: %d' % (sell_book.shape[0], len(positions)))
             return
 
+        self.logger.info('Start to track live market price and follow sell book to sell.')
         while self.is_market_open():
             for symbol in symbols:
                 try:
-                    self.trade_target_price(positions[symbol], sell_book[symbol])
+                    self.trade_target_price(positions[symbol], sell_book['low'][symbol], sell_book['high'][symbol])
                 except LoginFailed as e:
                     self.logger.error("failed to update sell order %d for %s: %s" % (sell_book[symbol], symbol, e))
                     self.robinhood.login()
@@ -57,23 +58,28 @@ class RobinhoodDayRunner:
         current_time_est = datetime.datetime.now(pytz.timezone('US/Eastern'))
         return 9 <= current_time_est.hour < 16
 
-    def trade_target_price(self, position, target_price: float):
-        bid_prices = self.robinhood.bid_price(position['symbol'])
-        max_bid, _ = max(bid_prices, key=lambda bids: bids[0])
-        max_bid = float(max_bid)
+    def trade_target_price(self, position, low_target: float, high_target: float):
+        max_trade_price = self.robinhood.get_max_trade_price(position['symbol'])
         cost_basis = float(position['average_buy_price'])
         shares = int(float(position['quantity']))
-        target_profit_pct = target_price / cost_basis - 1
-        target_profit = (target_price - cost_basis) * shares
+
         self.logger.info(
-            'Checking {0}, cost: ${1:.2f}, target: ${2:.2f}, current bid: ${3:.2f} ({4:.2%}, ${5:.2f})'.format(
-                position['symbol'], cost_basis, target_price, max_bid, max_bid / cost_basis - 1,
-                                                                       (max_bid - cost_basis) * shares))
-        if max_bid > target_price * 0.995:
+            'Checking {0}, cost: ${1:.2f}, target: [${2:.2f}, ${3:.2f}], '.format(position['symbol'], cost_basis,
+                                                                                  low_target, high_target) +
+            'current bid: ${0:.2f} ({1:.2%}, ${2:.2f})'.format(max_trade_price, max_trade_price / cost_basis - 1,
+                                                               (max_trade_price - cost_basis) * shares))
+        if max_trade_price > high_target * 0.995:
+            # place high target sell order
             self.robinhood.cancel_all_orders(position['symbol'])
-            self.robinhood.place_stop_limit_sell_order(position, target_price)
+            self.robinhood.place_stop_limit_sell_order(position, high_target)
             self.logger.info('New sell order placed for {0} @ ${1:.2f} ({2:+.2%}, ${3:+.2f})'.format(
-                position['symbol'], target_price, target_profit_pct, target_profit))
+                position['symbol'], high_target, high_target / cost_basis - 1, (high_target - cost_basis) * shares))
+        elif max_trade_price < low_target * 1.01:
+            # place low target sell order
+            self.robinhood.cancel_all_orders(position['symbol'])
+            self.robinhood.place_stop_limit_sell_order(position, low_target)
+            self.logger.info('New sell order placed for {0} @ ${1:.2f} ({2:+.2%}, ${3:+.2f})'.format(
+                position['symbol'], low_target, low_target / cost_basis - 1, (high_target - cost_basis) * shares))
 
 
 if __name__ == '__main__':
